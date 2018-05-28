c74d008e571cb7f8ade1562cdfaac1ab38353fa2
heo <heo9564@naver.com>
2018-05-28 22:44:40 +0900

    밤송이

---
+++ Bamsongi/Assets/BamsongiGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BamsongiGenerator : MonoBehaviour {
+    public GameObject bamsongi;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject go = Instantiate(bamsongi) as GameObject ;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            go.GetComponent<bamsongiControl>().Shoot(worldDir.normalized * 2000);
+        }
+	}
+}
---
+++ Bamsongi/Assets/bamsongiControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bamsongiControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        // Shoot(new Vector3(0, 200, 2000));
+	}
+
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
